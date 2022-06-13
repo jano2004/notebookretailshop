636c7f5c47a2b8dc2e9456b05322a4dfe5a75f85
jano2004 <wagnerjan463@gmail.com>
2022-06-13 15:00:08 +0200

    wip initial commit

---
+++ notebookretailshop/src/main/java/net/wagner/notebookretailshop/NotebookretailshopApplication.java
@@ -0,0 +1,13 @@
+package net.wagner.notebookretailshop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NotebookretailshopApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NotebookretailshopApplication.class, args);
+	}
+
+}
