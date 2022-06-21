a52799cad406c2ef16f99ae205167e0d04979a73
jano2004 <wagnerjan463@gmail.com>
2022-06-21 11:19:28 +0200

    code changes(beta)

--- notebookretailshop/src/main/java/net/wagner/notebookretailshop/MainJava.java
+++ notebookretailshop/src/main/java/net/wagner/notebookretailshop/MainJava.java
@@ -3,22 +3,73 @@ import java.util.Scanner;
 
 public class MainJava {
 	static int bestand;
-	static boolean bestellung;
+	static boolean bestellung = false;
+	static String groesse;
+	
+	
 	public static void main(String[] args) {
-		Data d1 = new Data();
-		bestand = d1.groesse.size(); 
+		Data d = new Data();
+		int bestand = d.groesse.size();
+		boolean moeglich = Bestellungmoeglich(bestand);
+		boolean janein = Bestellungjanein(moeglich);
+		int groessevorhanden = Groessevorhanden(janein);
+		Removegroesse(groessevorhanden);
 		
-		Scanner in = new Scanner(System.in);
-		System.out.println("Bestand: " + bestand + "\nMöchtest du einen Schuh bestellen? J/N: " );
-
-		String input = in.nextLine();
-		in.close();
-		
-		if(input.contains("J")){
-			System.out.println("Welche Schuhgroesse möchtest du bestellen: ");
+	}
+	public static boolean Bestellungmoeglich(int bestand) {
+		if(bestand > 0) {
+			return true;
 		}
-		else if(input.contains("N")) {
-			System.out.println("O.K.");
+		else {
+			return false;
 		}
 	}
+	
+	
+	public static boolean Bestellungjanein(boolean moeglich) {
+		if(moeglich) {
+			Scanner input = new Scanner(System.in);
+			System.out.println("Möchtest du einen Schuh bestellen? J/N:");
+			String janein = input.next();
+			input.close();
+			if(janein.contains("J")) {
+				moeglich = true;
+			}
+			else if(janein.contains("N")) {
+				moeglich = false;
+			}
+			else {
+				moeglich = false;
+			}
+		}
+		return moeglich;
+	}
+	
+	
+	public static int Groessevorhanden(boolean janein) {
+		Data d = new Data();
+		int groesse = 0;
+		if(janein) {
+			Scanner input = new Scanner(System.in);
+			System.out.println("Möchtest du einen Schuh bestellen? J/N:");
+			groesse = input.nextInt();
+			input.close();
+				if(!d.groesse.contains(groesse)) {
+					groesse = 0;
+				}
+		}
+		return groesse;
+	}
+	
+	
+	public static void Removegroesse(int groessevorhanden) {
+		Data d = new Data();
+		for(int i = 0; i < d.groesse.size(); i++) {
+			if(d.groesse.get(i) == groessevorhanden) {
+				d.groesse.remove(i);
+				i = d.groesse.size();
+			}
+		}
+	}
+	
 }
