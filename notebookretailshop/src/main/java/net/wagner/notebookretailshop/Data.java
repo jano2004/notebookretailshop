a165e1806468bc02ea6f784e8c5f44f6a9dee1f0
jano2004 <wagnerjan463@gmail.com>
2022-06-21 11:19:51 +0200

    second push(code update)

--- notebookretailshop/src/main/java/net/wagner/notebookretailshop/Data.java
+++ notebookretailshop/src/main/java/net/wagner/notebookretailshop/Data.java
@@ -3,27 +3,27 @@ package net.wagner.notebookretailshop;
 import java.util.ArrayList;
 
 public class Data {
-	ArrayList<String> groesse = new ArrayList<>();{
+	ArrayList<Integer> groesse = new ArrayList<>();{
 
 	for(int o = 0; o < 20; o++)
 	{
-		groesse.add("41");
+		groesse.add(41);
 	}
 	for(int p = 0; p < 20; p++)
 	{
-		groesse.add("42");
+		groesse.add(42);
 	}
 	for(int n = 0; n < 20; n++)
 	{
-		groesse.add("43");
+		groesse.add(43);
 	}
 	for(int l = 0; l < 20; l++)
 	{
-		groesse.add("44");
+		groesse.add(44);
 	}
 	for(int a = 0; a < 20; a++)
 	{
-		groesse.add("45");
+		groesse.add(45);
 		
 	 	}
 	}
